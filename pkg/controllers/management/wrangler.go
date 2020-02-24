c6e749d5950a87e1c63958240554e8e6d556b8c4
Dax McDonald <dax@rancher.com>
2020-02-24 13:44:39 -0700

    WIP for wrangler plan controller

--- pkg/controllers/management/wrangler.go
+++ pkg/controllers/management/wrangler.go
@@ -4,10 +4,18 @@ import (
 	"context"
 
 	"github.com/rancher/rancher/pkg/clustermanager"
+	"github.com/rancher/rancher/pkg/controllers/management/k3sUpgrade"
 	"github.com/rancher/rancher/pkg/wrangler"
 	"github.com/rancher/types/config"
+	"github.com/sirupsen/logrus"
 )
 
 func RegisterWrangler(ctx context.Context, wranglerContext *wrangler.Context, management *config.ManagementContext, manager *clustermanager.Manager) {
 	// Add controllers to register here
+
+	err := k3sUpgrade.Register(ctx, wranglerContext, management, manager)
+	if err != nil {
+		logrus.Fatal("Boom")
+	}
+
 }
