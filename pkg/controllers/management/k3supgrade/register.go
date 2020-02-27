ae64e91a862c25c07448c2449f97fd66e8595b4c
Dax McDonald <dax@rancher.com>
2020-02-27 12:10:40 -0700

    WIP for controllers

--- pkg/controllers/management/k3supgrade/register.go
+++ pkg/controllers/management/k3supgrade/register.go
@@ -2,14 +2,15 @@ package k3supgrade
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/rancher/rancher/pkg/clustermanager"
 	"github.com/rancher/rancher/pkg/systemaccount"
 	"github.com/rancher/rancher/pkg/wrangler"
 	wranglerv3 "github.com/rancher/rancher/pkg/wrangler/generated/controllers/management.cattle.io/v3"
-	"github.com/rancher/system-upgrade-controller/pkg/generated/clientset/versioned/typed/upgrade.cattle.io/v1"
+	planClientset "github.com/rancher/system-upgrade-controller/pkg/generated/clientset/versioned/typed/upgrade.cattle.io/v1"
 	v3 "github.com/rancher/types/apis/management.cattle.io/v3"
-	v32 "github.com/rancher/types/apis/project.cattle.io/v3"
+	projectv3 "github.com/rancher/types/apis/project.cattle.io/v3"
 	"github.com/rancher/types/config"
 	"github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -20,14 +21,16 @@ type handler struct {
 	systemUpgradeNamespace string
 	newVersion             string //TODO make this info.Version
 	clusterCache           wranglerv3.ClusterCache
-	apps                   v32.AppInterface
-	appLister              v32.AppLister
+	apps                   projectv3.AppInterface
+	appLister              projectv3.AppLister
 	templateLister         v3.CatalogTemplateLister
 	systemAccountManager   *systemaccount.Manager
 	manager                *clustermanager.Manager
 }
 
 const systemUpgradeNS = "system-upgrade"
+const rancherManagedPlan = "rancher-managed"
+const upgradeDisableLabelKey = "plan.upgrade.cattle.io/disable"
 
 func Register(ctx context.Context, wContext *wrangler.Context, mgmtCtx *config.ManagementContext, manager *clustermanager.Manager) error {
 	h := &handler{
@@ -45,48 +48,64 @@ func Register(ctx context.Context, wContext *wrangler.Context, mgmtCtx *config.M
 	return nil
 }
 
-func (h *handler) onClusterChange(key string, cluster *v3.Cluster) (*v3.Cluster, error) {
-
-	if cluster == nil { //can this occur?
-		return nil, nil
-	}
-
-	// only applies to k3s clusters
-	if cluster.Status.Driver != v3.ClusterDriverK3s {
-		return cluster, nil
-	}
+// deployPlans creates a master and worker plan in the downstream cluster to instrument
+// the system-upgrade-controller in the downstream cluster
+func (h *handler) deployPlans(cluster v3.Cluster) error {
 
 	// access downstream cluster
 	clusterCtx, err := h.manager.UserContext(cluster.Name)
 	if err != nil {
-		return cluster, err
+		return err
 	}
 
 	// create a client for GETing Plans in the downstream cluster
 	// TODO: We shouldn't create one every time
-
-	planClient, err := v1.NewForConfig(&clusterCtx.RESTConfig)
+	planConfig, err := planClientset.NewForConfig(&clusterCtx.RESTConfig)
 	if err != nil {
-		return cluster, err
+		return err
 	}
+	planClient := planConfig.Plans(h.systemUpgradeNamespace)
 
-	planList, err := planClient.Plans(h.systemUpgradeNamespace).List(metav1.ListOptions{})
+	planList, err := planClient.List(metav1.ListOptions{})
 	if err != nil {
 		// may need to handle this error, if there is no Plan CRD what should we do?
-
 		if errors.IsNotFound(err) {
 			// no plan CRD exists
+			logrus.Warnf("plan CRD does not exist: %s", err)
 		}
-		logrus.Warnf("err getting plans %s", err)
-		return cluster, err
+		return err
 	}
 
-	for plan := range planList.Items {
-		fmt.Println("Found a plan")
-		fmt.Printf("%+v\n", plan)
+	// deactivate all existing plans that are not managed by Rancher
+	for _, plan := range planList.Items {
+		if _, ok := plan.Labels[rancherManagedPlan]; !ok {
+			// inverse selection is used here, we select a non-existent label
+			plan.Spec.NodeSelector.MatchExpressions = []metav1.LabelSelectorRequirement{{
+				Key:      upgradeDisableLabelKey,
+				Operator: metav1.LabelSelectorOpExists,
+			}}
+
+			_, err := planClient.Update(&plan)
+			if err != nil {
+				return err
+			}
+		}
 	}
 
-	fmt.Println("Cluster has changed OwO")
+	// apply master and worker plans
+	// TODO: what if they already exist?
+	masterPlan, err := generateMasterPlan(cluster.Spec.K3sConfig.Version.String(), cluster.Spec.K3sConfig.ServerConcurrency)
+	_, err = planClient.Create(&masterPlan)
+	if err != nil {
+		return err
+	}
+	workerPlan, err := generateWorkerPlan(cluster.Spec.K3sConfig.Version.String(),
+		cluster.Spec.K3sConfig.WorkerConcurrency)
+	_, err = planClient.Create(&workerPlan)
+	if err != nil {
+		return nil
+	}
+	fmt.Println("Deployed plans into cluster")
 
-	return cluster, nil
+	return nil
 }
--- pkg/controllers/management/k3supgrade/template.go
+++ pkg/controllers/management/k3supgrade/template.go
@@ -1,10 +1,17 @@
 package k3supgrade
 
+import (
+	"github.com/rancher/system-upgrade-controller/pkg/apis/upgrade.cattle.io"
+	planv1 "github.com/rancher/system-upgrade-controller/pkg/apis/upgrade.cattle.io/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/kubectl/pkg/describe"
+)
+
 const k3sMasterPlan = `---
 apiVersion: upgrade.cattle.io/v1
 kind: Plan
 metadata:
-  name: k3s-latest
+  name: k3s-master-plan 
   namespace: system-upgrade
 spec:
   concurrency: 1
@@ -16,4 +23,92 @@ spec:
   drain:
     force: true
   upgrade:
+    image: rancher/k3s-upgrade:latest`
+
+// https://github.com/rancher/k3s-upgrade/pull/9
+const k3sWorkerPlan = `---
+apiVersion: upgrade.cattle.io/v1
+kind: Plan
+metadata:
+  name: k3s-worker-plan
+  namespace: system-upgrade
+spec:
+  concurrency: 1
+  version: v1.17.2-k3s1
+  # The prepare init container is run before cordon/drain which is run before the upgrade container.
+  # Shares the same format as the "upgrade" container
+  prepare:
+     image: rancher/k3s-upgrade:latest
+     args: ["prepare","k3s-master-plan"]
+  nodeSelector:
+    matchExpressions:
+    - {key: k3s-worker-upgrade, operator: Exists}
+  serviceAccountName: system-upgrade
+  drain:
+    force: true
+  upgrade:
     image: rancher/k3s-upgrade`
+
+const k3sMasterPlanName = "k3s-master-plan"
+const k3sWorkerPlanName = "k3s-worker-plan"
+const systemUpgradeServiceAccount = "system-upgrade"
+const upgradeImage = "rancher/k3s-upgrade:latest"
+
+var genericPlan = planv1.Plan{
+	TypeMeta: metav1.TypeMeta{
+		Kind:       "Plan",
+		APIVersion: upgrade.GroupName + `/v1`,
+	},
+	ObjectMeta: metav1.ObjectMeta{
+		Namespace: systemUpgradeNS,
+		Labels:    map[string]string{rancherManagedPlan: "true"},
+	},
+	Spec: planv1.PlanSpec{
+		Concurrency:        0,
+		NodeSelector:       nil,
+		ServiceAccountName: systemUpgradeServiceAccount,
+		Channel:            "",
+		Version:            "",
+		Secrets:            nil,
+		Prepare:            nil,
+		Cordon:             false,
+		Drain: &planv1.DrainSpec{
+			Force: true,
+		},
+		Upgrade: &planv1.ContainerSpec{
+			Image: upgradeImage,
+		},
+	},
+	Status: planv1.PlanStatus{},
+}
+
+func generateMasterPlan(version string, concurrency int) (planv1.Plan, error) {
+	masterPlan := genericPlan
+	masterPlan.Name = k3sMasterPlanName
+	masterPlan.Spec.Version = version
+	masterPlan.Spec.Concurrency = int64(concurrency)
+	// only select master nodes
+	masterPlan.Spec.NodeSelector.MatchExpressions = []metav1.LabelSelectorRequirement{{
+		Key:      describe.LabelNodeRolePrefix + "master",
+		Operator: metav1.LabelSelectorOpIn,
+		Values:   []string{"true"},
+	}}
+
+	return masterPlan, nil
+}
+
+func generateWorkerPlan(version string, concurrency int) (planv1.Plan, error) {
+	workerPlan := genericPlan
+	workerPlan.Name = k3sWorkerPlanName
+	workerPlan.Spec.Version = version
+	workerPlan.Spec.Concurrency = int64(concurrency)
+
+	// worker plans wait for master plans to complete
+	workerPlan.Spec.Prepare = &planv1.ContainerSpec{
+		Image:   upgradeImage,
+		Command: []string{"prepare", k3sMasterPlanName},
+		Args:    nil,
+	}
+
+	return workerPlan, nil
+}
